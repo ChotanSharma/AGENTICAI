eda204a6c5967dbcd33b0b8a53346b64571ca8da
ChotanSharma <chotansharma@yahoo.com>
2026-01-02 03:59:19 -0500

    fix the bug in tool_functions dict

--- fileFindAgent.py
+++ fileFindAgent.py
@@ -41,9 +41,10 @@ def terminate(message: str) -> None:
 tool_functions = {
     "list_files": list_files,
     "read_file": read_file,
-    "terminate_agent": terminate
+    "terminate": terminate
 }
 
+# Define the tools with their specifications
 tools = [
     {
         "type": "function",
@@ -82,3 +83,73 @@ tools = [
         }
     }
 ]
+
+# Define the agent's system rules
+agent_rules = [
+    {
+        "role": "system",
+        "content": 
+        """
+You are a file management agent that can list and read files in the current directory.
+You must use the provided functions to interact with the file system.
+Follow these rules strictly:
+When you are done, terminate the conversation by using the "terminate" tool 
+and I will provide the results to the user.
+"""
+    }
+]
+
+#Initialize conversation with system rules
+iterations = 0
+max_iterations = 10
+
+user_task = input("What would you like to do with the agent?").strip()
+
+memory = [{"role": "user", "content":user_task}]
+
+# Agent interaction loop
+while iterations < max_iterations:
+    messages = agent_rules + memory
+
+    response = completion(
+        model="openai/gpt-4o",
+        messages=messages,
+        tools=tools,
+        max_tokens=1024,
+        api_key=OPENAI_API_KEY
+    )
+
+    if response.choices[0].message.tool_calls:
+        tool = response.choices[0].message.tool_calls[0]
+        tool_name = tool.function.name
+        tool_args = json.loads(tool.function.arguments)
+
+        action ={
+            "tool_name": tool_name,
+            "args": tool_args
+        }
+
+        if tool_name == "terminate":
+            print(f"Termination message: {tool_args['message']}")
+            break
+        elif tool_name in tool_functions:
+            try: 
+                result = {"result": tool_functions[tool_name](**tool_args)}
+            except Exception as e:
+                result = {"error": f"Error executing tool '{tool_name}': {str(e)}"}
+        else:
+            result = {"error": f"Tool '{tool_name}' not recognized."}
+
+        print(f"Executing tool: {tool_name} with arguments {tool_args}")
+        memory.extend([
+            {"role": "assistant",
+             "content": json.dumps(action)},
+             {"role": "user", "content": json.dumps(result)}
+        ])
+    else:
+        result = response.choices[0].message.content
+        print(f"Agent response: {result}")
+        break
+    iterations += 1
+
+
